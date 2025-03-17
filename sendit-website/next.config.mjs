01961626c137bf90a36fd9fd8feb484d6776e18f
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2025-03-17 04:48:01 +0000

    changed the next.config.mjs file

--- sendit-website/next.config.mjs
+++ sendit-website/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // ✅ Allow <Image /> to work with static export
+  },
 };
 
 export default nextConfig;
